3c605f6019bfdffabc1aebb538d12b6b85e30b84
haw <haw358linux@163.com>
2017-08-11 15:24:07 +0800

    network classloader

---
+++ src/main/java/top/haw358/utils/classloader/ClassLoaderTest.java
@@ -0,0 +1,27 @@
+package top.haw358.utils.classloader;
+
+/**
+ * Created by haw on 17-8-11.
+ */
+public class ClassLoaderTest {
+    public static void main(String[] args) {
+        try {
+            /*ClassLoader loader = ClassLoaderTest.class.getClassLoader();  //获得ClassLoaderTest这个类的类加载器
+            while(loader != null) {
+                System.out.println(loader);
+                loader = loader.getParent();    //获得父加载器的引用
+            }
+            System.out.println(loader);*/
+
+
+            String rootUrl = "http://localhost:8080/httpweb/classes";
+            NetworkClassLoader networkClassLoader = new NetworkClassLoader(rootUrl);
+            String classname = "org.classloader.simple.NetClassLoaderTest";
+            Class clazz = networkClassLoader.loadClass(classname);
+            System.out.println(clazz.getClassLoader());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
